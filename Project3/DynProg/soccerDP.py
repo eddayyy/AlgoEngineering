7e5da05811a0ad0d59de470b70bc4241cff77e1c
eddayyy <eduardonunez@csu.fullerton.edu>
2023-04-19 13:08:01 -0700

    Added comments to ES and DP solutions

--- Project3/DynProg/soccerDP.py
+++ Project3/DynProg/soccerDP.py
@@ -1,45 +1,91 @@
+# Authors: Eduardo Nunez | Juan Gonzalez
+# Authors emails: eduardonunez@csu.fullerton.edu | gonzalez.juanant524@csu.fullerton.edu 
 import time
+import random
 
+# ============= SOCCEER ALGORITHM O(n^2) =============
 def soccer_dyn_prog(F):
+    # Get the number of rows and columns in the grid
     r = len(F)
     c = len(F[0])
 
+    # Check if the starting position is an obstacle, return 0 if it is
     if F[0][0] == 'X':
         return 0
 
+    # Initialize the 2D list to store the number of ways to reach each position
     A = [[0] * c for _ in range(r)]
+    # Set the value of the starting position to 1
     A[0][0] = 1
 
+    # Loop through each position in the grid
     for i in range(r):
         for j in range(c):
+            # Check if the current position is an obstacle, skip if it is
             if F[i][j] == 'X':
                 A[i][j] = 0
                 continue
+            # Calculate the number of ways to reach the current position
+            # by summing the number of ways to reach the position above and
+            # the position to the left (if they are open spaces)
             above = A[i - 1][j] if i > 0 and F[i - 1][j] == '.' else 0
             left = A[i][j - 1] if j > 0 and F[i][j - 1] == '.' else 0
 
+            # Store the total number of ways to reach the current position in the list
             A[i][j] += above + left
 
+    # Return the number of ways to reach the bottom-right corner of the grid
     return A[r - 1][c - 1]
 
+
+# ============= RANDOM GRID =============
+def create_random_grid(n):
+    grid = [['.' for _ in range(n)] for _ in range(n)]  # Create an empty n x n grid filled with '.'
+
+    for i in range(n):
+        for j in range(n):
+            if random.random() < 0.2:  # 20% chance to place 'X' in a cell
+                grid[i][j] = 'X'
+
+    grid[0][0] = '.'      # Ensure the starting point is not blocked
+    grid[n-1][n-1] = '.'  # Ensure the end point is not blocked
+
+    return grid
+
+# ============= MAIN IMPLEMENTATION =============
 def main():
-    grid = [
-        ['.', '.', '.', '.', '.', '.', 'X', '.', 'X'],
-        ['X', '.', '.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', 'X', '.', '.', '.', 'X', '.'],
-        ['.', '.', 'X', '.', '.', '.', '.', 'X', '.'],
-        ['.', 'X', '.', '.', '.', '.', 'X', '.', '.'],
-        ['.', '.', '.', '.', 'X', '.', '.', '.', '.'],
-        ['.', '.', 'X', '.', '.', '.', '.', '.', 'X'],
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.']
-    ]
+    ns = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] # Varying sizes of n
+    timings = []
+    
+    for n in ns: # Create an r x c grid for each n value in ns
+        grid = create_random_grid(n)    # Use our random grid generator to get random valid and invalid points
 
-    start_time = time.time()
-    num_paths = soccer_dyn_prog(grid)
-    end_time = time.time()
+        start_time = time.time()            # Get the start time
+        num_paths = soccer_dyn_prog(grid)   # Call the Soccer Avoidance DP Solution
+        end_time = time.time()              # Get the end time
 
-    print("Number of different paths (dynamic programming):", num_paths)  # Output: 102
-    print("Runtime of the soccer_dyn_prog function:", (end_time - start_time))
+        runtime = (end_time - start_time)  * 1000 # Calculate runtime in milliseconds
+        timings.append(runtime)             # Add the runtime of the Soccer Avoidance Algorithm for the nth element in ns
+
+        print(f"Size: {n}x{n}, Number of different paths: {num_paths}, Runtime: {runtime:.2f} ms")
+        
+    # ============= EXAMPLE INPUT IMPLEMENTATION =============
+    #
+    # TO RUN THIS CORRECTLY, COMMENT OUT ALL OTHER LINES IN THE MAIN FUNCTION & UNCOMMENT THE CODE BELOW SECTION
+    #
+    # grid = [
+    #     ['.', '.', '.', '.', '.', '.', 'X', '.', 'X'],
+    #     ['X', '.', '.', '.', '.', '.', '.', '.', '.'],
+    #     ['.', '.', '.', 'X', '.', '.', '.', 'X', '.'],
+    #     ['.', '.', 'X', '.', '.', '.', '.', 'X', '.'],
+    #     ['.', 'X', '.', '.', '.', '.', 'X', '.', '.'],
+    #     ['.', '.', '.', '.', 'X', '.', '.', '.', '.'],
+    #     ['.', '.', 'X', '.', '.', '.', '.', '.', 'X'],
+    #     ['.', '.', '.', '.', '.', '.', '.', '.', '.']
+    # ]
+    
+    # num_paths = soccer_dyn_prog(grid)
+    # print("Number of different paths (exhaustive search):", num_paths)  # Output: 102
 
 if __name__ == "__main__":
     main()
--- Project3/ExhaustiveSearch/soccerES.py
+++ Project3/ExhaustiveSearch/soccerES.py
@@ -1,26 +1,46 @@
+# Authors: Eduardo Nunez | Juan Gonzalez
+# Authors emails: eduardonunez@csu.fullerton.edu | gonzalez.juanant524@csu.fullerton.edu 
+import random
+import time
+
+# ============= SOCCEER ALGORITHM O(n * 2^n) =============
+# This function checks if a given candidate path is valid, given a field with obstacles
 def is_valid(candidate, field):
+    # Get the dimensions of the field
     rows, cols = len(field), len(field[0])
+    # Set the starting row and column to (0, 0)
     row, col = 0, 0
 
+    # Loop through each move in the candidate path
     for move in candidate:
+        # Update the row and column based on the move
         if move == 'R':
             col += 1
         else:
             row += 1
 
+        # Check if the candidate path goes out of bounds or hits an obstacle
         if row >= rows or col >= cols or field[row][col] == 'X':
             return False
 
+    # Check if the candidate path reaches the bottom-right corner of the field
     return row == rows - 1 and col == cols - 1
 
 
+# This function counts the number of valid paths from the top-left to bottom-right corner of the field,
+# by generating all possible paths and checking their validity
 def count_paths_exhaustive(field):
+    # Get the dimensions of the field
     rows, cols = len(field), len(field[0])
+    # Calculate the length of the path needed to traverse the field
     path_length = rows + cols - 2
+    # Initialize a counter for the number of valid paths
     counter = 0
 
+    # Loop through all possible binary strings of length path_length
     for bits in range(0, 2 ** path_length):
         candidate = []
+        # Convert each binary digit to a move in the candidate path
         for k in range(path_length):
             bit = (bits >> k) & 1
             if bit == 1:
@@ -28,26 +48,62 @@ def count_paths_exhaustive(field):
             else:
                 candidate.append('D')
 
+        # Check if the candidate path is valid, and increment the counter if it is
         if is_valid(candidate, field):
             counter += 1
 
+    # Return the number of valid paths
     return counter
 
 
-def main():
-    grid = [
-        ['.', '.', '.', '.', '.', '.', 'X', '.', 'X'],
-        ['X', '.', '.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', 'X', '.', '.', '.', 'X', '.'],
-        ['.', '.', 'X', '.', '.', '.', '.', 'X', '.'],
-        ['.', 'X', '.', '.', '.', '.', 'X', '.', '.'],
-        ['.', '.', '.', '.', 'X', '.', '.', '.', '.'],
-        ['.', '.', 'X', '.', '.', '.', '.', '.', 'X'],
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.']
-    ]
 
-    num_paths = count_paths_exhaustive(grid)
-    print("\nNumber of different paths (exhaustive search):", num_paths)  # Output: 102
+# ============= RANDOM GRID =============
+def create_random_grid(n):
+    grid = [['.' for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if random.random() < 0.2:  # 20% chance to place 'X' in a cell
+                grid[i][j] = 'X'
+
+    grid[0][0] = '.'  # Ensure the starting point is not blocked
+    grid[n-1][n-1] = '.'  # Ensure the end point is not blocked
+
+    return grid
+
+# ============= MAIN IMPLEMENTATION =============
+def main():
+    ns = [2, 4, 5, 6, 7, 8, 9, 10] # Varying sizes of n
+    timings = []
+
+    for n in ns:
+        grid = create_random_grid(n)
+
+        start_time = time.time()
+        num_paths = count_paths_exhaustive(grid)
+        end_time = time.time()
+
+        runtime = (end_time - start_time) * 1000 # Calculate runtime in milliseconds
+        timings.append(runtime)
+
+        print(f"Size: {n}x{n}, Number of different paths: {num_paths}, Runtime: {runtime:.2f} ms")
+
+    # ============= EXAMPLE INPUT IMPLEMENTATION =============
+    #
+    # TO RUN THIS CORRECTLY, COMMENT OUT ALL OTHER LINES IN THE MAIN FUNCTION & UNCOMMENT THIS SECTION
+    #
+    # grid = [
+    #     ['.', '.', '.', '.', '.', '.', 'X', '.', 'X'],
+    #     ['X', '.', '.', '.', '.', '.', '.', '.', '.'],
+    #     ['.', '.', '.', 'X', '.', '.', '.', 'X', '.'],
+    #     ['.', '.', 'X', '.', '.', '.', '.', 'X', '.'],
+    #     ['.', 'X', '.', '.', '.', '.', 'X', '.', '.'],
+    #     ['.', '.', '.', '.', 'X', '.', '.', '.', '.'],
+    #     ['.', '.', 'X', '.', '.', '.', '.', '.', 'X'],
+    #     ['.', '.', '.', '.', '.', '.', '.', '.', '.']
+    # ]
+    # num_paths = count_paths_exhaustive(grid)
+    # print("Number of different paths (exhaustive search):", num_paths)  # Output: 102
 
 
 if __name__ == "__main__":
